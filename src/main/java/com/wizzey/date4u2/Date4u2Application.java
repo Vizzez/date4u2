e7541188af7313bbc7db14dc42a9a2308f4cb797
Wladislaw Wellmann <wladislawwellmann@gmail.com>
2024-08-29 20:15:43 +0200

    initial

---
+++ src/main/java/com/wizzey/date4u2/Date4u2Application.java
@@ -0,0 +1,13 @@
+package com.wizzey.date4u2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Date4u2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Date4u2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/wizzey/date4u2/Date4u2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizzey.date4u2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Date4u2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
